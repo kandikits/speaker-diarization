0aa8aaa4b9bf09f5693bbecee99148df66725e6c
kandikits <95465112+kandikits@users.noreply.github.com>
2021-12-30 18:22:38 +0530

    Update kit_installer.bat

--- kit_installer.bat
+++ kit_installer.bat
@@ -12,7 +12,7 @@ SET PY_LOCATION="C:\Python"
 SET PY_VERSION=3.9.8
 SET PY_DOWNLOAD_URL=https://www.python.org/ftp/python/3.9.8/python-3.9.8-amd64.exe
 SET REPO_DOWNLOAD_URL=https://github.com/kandikits/speaker-diarization/releases/download/speaker-diarization-master.zip
-SET REPO_DEPENDENCIES_URL=https://raw.githubusercontent.com/kandikits/speaker-diarization/master/requirements.txt
+SET REPO_DEPENDENCIES_URL=https://raw.githubusercontent.com/kandikits/speaker-diarization/main/requirements.txt
 SET REPO_NAME=speaker-diarization.zip
 where /q python
 IF ERRORLEVEL 1 (
